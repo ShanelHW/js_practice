28d483c55057b83e245bfd9488520fe462aa2c59
Shanel <shanelhickmanwhitmore@gmail.com>
2022-08-25 13:13:24 -0400

    Create while.js

---
+++ while.js
@@ -0,0 +1,19 @@
+//Fundamental: While Loops
+
+let count =2
+while (count<8) {
+    console.log(count)
+    count+= 2
+    
+}
+
+
+//Conditional: While Loops
+let addThis = 0
+let sum = 0
+
+while (addThis < 10) {
+    sum += addThis
+    addThis++
+}
+console.log(sum)
