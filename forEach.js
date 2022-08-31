c340fe3a1b58ab6e54ff9e19bba80396f91628de
Shanel <shanelhickmanwhitmore@gmail.com>
2022-08-31 10:53:56 -0400

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,22 +1,24 @@
-let alphabet = '';
-const letters = ['a', 'b', 'c', 'd'];
-function forEach(letters) {
-  alphabet += char;
-}
-console.log(alphabet); //prints 'abcd'
-
-const map = (arr, forEach) => {
-   forEach(letters)
+function recreateforEach (array , callback)
+{
+    for(i=0;i<array.length;i++ ){
+       callback(array[i])
     }
-    return alphabet;
-    console.log(alphabet)
+}
+const recreateMap = (array, callback) => {
+    let newArr = []
+        recreateforEach(array, function (item){
+        newArr.push(callback (item))
+    })
+    return newArr
+}
+
 
 
 
 // ADD CODE HERE
 
 // Uncomment these to check your work!
- console.log(typeof forEach); // should log: 'function'
- forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
-console.log(typeof map); // should log: 'function'
-console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
+//console.log(typeof forEach); // should log: 'function'
+recreateforEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
+console.log(typeof recreateMap); // should log: 'function'
+console.log(recreateMap([3,4,5], n => n - 2)); // should log: [1, 2, 3]
