dfb8a14aeb7e5798302808c0a7ddbccbfd814cc9
Shanel <shanelhickmanwhitmore@gmail.com>
2022-08-31 09:20:57 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,12 +1,13 @@
-const subtractTwo = (number) => number - 2
+const subtractTwo = (number) => number - 2;
 
-function map (arr, subtractTwo) {
-    for(let i = 0; i < arr.length; i++){
- arr[i] = subtractTwo(arr[i])    }
-    return arr;
+function map(arr, subtractTwo) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = subtractTwo(arr[i]);
+  }
+  return arr;
 }
 
 // Uncomment these to check your work!
 console.log(typeof subtractTwo); // should log: 'function'
 console.log(typeof map); // should log: 'function'
-console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
+console.log(map([3, 4, 5], subtractTwo)); // should log: [ 1, 2, 3 ]
