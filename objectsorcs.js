262efcc06779197879676d8f16df3322d76577f7
Shanel <shanelhickmanwhitmore@gmail.com>
2022-09-01 15:29:43 -0400

    Create objectsorcs.js

---
+++ objectsorcs.js
@@ -0,0 +1,49 @@
+const obj = {
+    contents: ["bits", "pieces", "springs", "sprockets"]
+}
+
+for(let i = 0; i<= obj.contents.length; i++){
+    console.log (`This object has ${obj.contents[i]}`)
+}
+
+const store = {
+    storename: "ToysArentUs",
+    address: 123 Main St,
+    toy : {
+        name: `Serf`, 
+        type:`ball`, 
+        color: `red`}
+}
+
+const corporation = {
+    storename: "BlicksSportingGoods",
+   const needRestock= {
+    console.log(`There are ${corporation.items[0].quantity} `)
+        store.push(items)
+    },
+    address: 987 Center Ln,
+    items : {
+        name: `Nalding`, 
+        type:`racquet`, 
+        color: `pink`,
+        price:``,
+        description: {
+            hasAluminum: true,
+            hasWood: false,
+            hasNylonStrings: true,
+        },
+        quantity: 500,
+        }
+        {
+            name: `Nalding`, 
+            type:`ball`, 
+            color: `red/white/blue`,
+            price:``,
+            description: {
+                hasAluminum: true,
+                hasWood: false,
+                hasNylonStrings: true,
+            },
+            quantity: 500,
+        }
+}
