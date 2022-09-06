9df92bf50393837fc85c04c218eef629a5ece5a8
Shanel <shanelhickmanwhitmore@gmail.com>
2022-09-06 16:00:07 -0400

    Update w2d3.js

--- w2d3.js
+++ w2d3.js
@@ -9,13 +9,18 @@
 //         this.gender = gender;
 //         this.height = height;
 //         this.weight = weight;
-//         this.feelings = feelings;
 //     }
 //     feels() {
+//         //adds ability to emote
 //         feelings = [happy, sad, confused, angry, amused];
 //     }
 //     walk(){
 //         let isWalking = true;
+//         let isSitting = false;
+//     }
+//     sits(){
+//         let isWalking = false;
+//         let isSitting = true;
 //     }
 //     lives(){
 //         let isAlive = true;
@@ -28,21 +33,21 @@
 
 
 // class PostalWorker extends Person{
-        // constructor(name, age, gender, height, weight){
-        //super(name, age, gender, height, weight)
-        // this.jobTitle = `Postal Worker`;
-        //     sortsMail(mail) {
+//         constructor(name, age, gender, height, weight){
+//         super(name, age, gender, height, weight)
+//         this.jobTitle = `Postal Worker`;}
+//     sortsMail(mail) {
 //         console.log(mail.sort())
 //     }
 //     deliversPackages(mail){
-//         (package.weight >= 3) ? console.log(`Delivers ${package.id} to mailbox`) : console.log(`Delivers ${package.id} to door`)
+//         (mail.weight >= 3) ? console.log(`Delivers ${mail.id} to mailbox`) : console.log(`Delivers ${mail.id} to door`)
 //     }
+
 // }
-//}
 // class Chef extends Person{
- // constructor(name, age, gender, height, weight){
-//super(name, age, gender, height, weight)
-// this.jobTitle = `Chef`;
+//  constructor(name, age, gender, height, weight){
+// super(name, age, gender, height, weight)
+// this.jobTitle = `Chef`;}
 //     cooks(food) {
 //         console.log(`Order Up`);
 //     }
@@ -50,31 +55,76 @@
 //         (ingredient) ? console.log(`${jobTitle} prepares ${ingredient} for lunch`) : console.log(`${ingredient} is out of stock. Unable to prepare`);
 //     }
 // }
-//}
 
-// let PW1 = new PostalWorker;
-// let PW2 = new PostalWorker;
 
-// let prepCook = new Chef;
-// let lineCook = new Chef;
+// let PW1 = new PostalWorker(`Peach`, 28, `F`, 5, 120);
+// let PW2 = new PostalWorker(`Yoshi`, 18, `M`, 3, 110);
+
+// console.log(PW1);
+// console.log(PW2);
+
+// let prepCook = new Chef(`Doc`, 54, `M`, `5'9`, 180);
+
+// let lineCook = new Chef(`Lemon`, 22, `F`, `5'1`, 160);
+
+// console.log(prepCook);
+// console.log(lineCook);
+
+
 
 //Hungry for More
-
 class BankAccount{
     constructor(){
     ownerName = ``;
     balance = 0;
     acctNum = `123456` + Math.random()*1000; 
     }
+    deposit(amount){
+        balance += amount;
+    }
+    withdraw(amount){
+        if (balance >= 0) {
+            balance -= amount;
+        } else {
+            console.log('Insufficient Funds')
+        }
+    }
+    checkBal(){
+        console.log(this.balance);
+    }
 }
 console.log(BankAccount)
 
 class checkingAccount extends BankAccount{
-
+constructor(){
+super(ownerName, balance, acctNum)
+ acctType = 'basic'
+}
+overdraftEnabled = true;
+withdraw(amount){
+    if (balance >= 0) {
+        balance -= amount;
+    } else if(balance <= 0 && overdraftEnabled) {
+        balance -= amount;
+        console.log('Overdraft protection used for withdrawl of ${amount}')
+    }else {
+        console.log('Insufficient funds')
+    }
+}
 }
 class savingsAccount extends BankAccount{
-    
-}
-class moneyMarketAccount extends BankAccount{
-    
-}
+    constructor(){
+        super(ownerName, balance, acctNum)
+         acctType = 'interest accruing'
+        }
+        withdraw(amount){
+                console.log(`Unable to process withdrawl for Savings Account ${acctNum}. Please contact your branch`)
+            }
+        }
+
+// class moneyMarketAccount extends BankAccount{
+//     constructor(){
+//         super(ownerName, balance, acctNum)
+//          acctType = 'interest accruing'
+//         }
+// }
